6c295bb72970d24b97647bb0ca93787f7c3f28c2
jwdietrich <jwdietrich@18aeb308-97f3-4495-a81e-6d4c78a8ff35>
2012-04-30 22:26:22 +0000

    First partly working version of one-way sensitivity analysis plot (some intended features still missing, however).

--- sensitivityanalysis.pas
+++ sensitivityanalysis.pas
@@ -28,6 +28,8 @@ type
 
   TSensitivityAnalysisForm = class(TForm)
     Chart1: TChart;
+    ChartAxisTransformations1: TChartAxisTransformations;
+    ChartAxisTransformations1LinearAxisTransform1: TLinearAxisTransform;
     CheckGroup1: TCheckGroup;
     TSHColorBox: TColorBox;
     FT4ColorBox: TColorBox;
@@ -80,26 +82,40 @@ begin
     begin {GD1}
       SensitivityAnalysisForm.MinSpinEdit.Value := GD1 / 3;
       SensitivityAnalysisForm.MaxSpinEdit.Value := GD1 * 3;
+      SensitivityAnalysisForm.ChartAxisTransformations1LinearAxisTransform1.Scale := 1e-9;
     end;
     1:
     begin {GD2}
       SensitivityAnalysisForm.MinSpinEdit.Value := GD2 / 3;
       SensitivityAnalysisForm.MaxSpinEdit.Value := GD2 * 3;
+      SensitivityAnalysisForm.ChartAxisTransformations1LinearAxisTransform1.Scale := 1e-15;
     end;
     2:
     begin {GT}
       SensitivityAnalysisForm.MinSpinEdit.Value := GT / 3;
       SensitivityAnalysisForm.MaxSpinEdit.Value := GT * 3;
+      SensitivityAnalysisForm.ChartAxisTransformations1LinearAxisTransform1.Scale := 1e-12;
     end
   end;
+end;
+
+procedure SetBottomAxisCaption;
+var
+  theCaption: String;
+begin
+  theCaption := SensitivityAnalysisForm.StrucParCombo.Text;
+  if theCaption = 'GD1' then theCaption := theCaption + ' (nmol/s)'
+  else if theCaption = 'GD2' then theCaption := theCaption + ' (fmol/s)'
+  else if theCaption = 'GT' then theCaption := theCaption + ' (pmol/s)';
+  SensitivityAnalysisForm.Chart1.BottomAxis.Title.Caption :=
+    theCaption;
 end;
 
 procedure DrawDummySensitivityPlot;
 {Draws an empty plot}
 begin
   SensitivityAnalysisForm.Chart1.LeftAxis.Title.Caption := 'Dependent Parameter';
-  SensitivityAnalysisForm.Chart1.BottomAxis.Title.Caption :=
-    SensitivityAnalysisForm.StrucParCombo.Text;
+  SetBottomAxisCaption;
   with FLine[1] do
   begin
     AddXY(0, 0, '', clBlack);
@@ -110,34 +126,44 @@ end;
 procedure DrawCurves(xPar: real);
 {Draws the curves in dependence from the independent parameter xPar}
 begin
+  SeriesCount := 0;
   if SensitivityAnalysisForm.CheckGroup1.Checked[0] then
   begin
     {TSH}
     FLine[1].AddXY(xPar, TSH1 * gParameterFactor[TSH_pos], '',
       SensitivityAnalysisForm.TSHColorBox.Selected);
+    SensitivityAnalysisForm.Chart1.LeftAxis.Title.Caption := 'TSH' + ': ' + gParameterUnit[TSH_pos];
+    inc(SeriesCount);
     FLine[1].SeriesColor := SensitivityAnalysisForm.TSHColorBox.Selected;
   end;
   if SensitivityAnalysisForm.CheckGroup1.Checked[1] then
   begin
     {FT3}
-    FLine[2].AddXY(xPar, FT41 * gParameterFactor[FT4_pos], '',
+    FLine[2].AddXY(xPar, FT41 / UFT4 * gParameterFactor[FT4_pos], '',
       SensitivityAnalysisForm.FT4ColorBox.Selected);
+    SensitivityAnalysisForm.Chart1.LeftAxis.Title.Caption := 'FT4' + ': ' + gParameterUnit[FT4_pos];
+    inc(SeriesCount);
     FLine[2].SeriesColor := SensitivityAnalysisForm.FT4ColorBox.Selected;
   end;
   if SensitivityAnalysisForm.CheckGroup1.Checked[2] then
   begin
     {FT3}
-    FLine[3].AddXY(xPar, FT31 * gParameterFactor[FT3_pos], '',
+    FLine[3].AddXY(xPar, FT31 / UFT3 * gParameterFactor[FT3_pos], '',
       SensitivityAnalysisForm.FT3ColorBox.Selected);
+    SensitivityAnalysisForm.Chart1.LeftAxis.Title.Caption := 'FT3' + ': ' + gParameterUnit[FT3_pos];
+    inc(SeriesCount);
     FLine[3].SeriesColor := SensitivityAnalysisForm.FT3ColorBox.Selected;
   end;
   if SensitivityAnalysisForm.CheckGroup1.Checked[3] then
   begin
     {cT3}
-    FLine[4].AddXY(xPar, T3z1 * gParameterFactor[cT3_pos], '',
+    FLine[4].AddXY(xPar, T3z1 / UFT3 * gParameterFactor[cT3_pos], '',
       SensitivityAnalysisForm.cT3ColorBox.Selected);
+    SensitivityAnalysisForm.Chart1.LeftAxis.Title.Caption := 'cT3' + ': ' + gParameterUnit[cT3_pos];
+    inc(SeriesCount);
     FLine[4].SeriesColor := SensitivityAnalysisForm.cT3ColorBox.Selected;
   end;
+  if SeriesCount > 1 then SensitivityAnalysisForm.Chart1.LeftAxis.Title.Caption := 'Dependent Parameters';
 end;
 
 procedure DrawOWSensitivityPlot(empty: boolean);
@@ -196,9 +222,7 @@ begin
         end
       end;
     end;
-    SensitivityAnalysisForm.Chart1.LeftAxis.Title.Caption := 'Dependent Parameter';
-    SensitivityAnalysisForm.Chart1.BottomAxis.Title.Caption :=
-      SensitivityAnalysisForm.StrucParCombo.Text;
+    SetBottomAxisCaption;
     {restore saved structure parameters:}
     GD1 := StoredParameters.GD1;
     GD2 := StoredParameters.GD2;
@@ -285,8 +309,7 @@ end;
 
 procedure TSensitivityAnalysisForm.StrucParComboChange(Sender: TObject);
 begin
-  SensitivityAnalysisForm.Chart1.BottomAxis.Title.Caption :=
-    SensitivityAnalysisForm.StrucParCombo.Text;
+  SetBottomAxisCaption;
   {ItemIndex is evaluated in the SetStrucParBoundaries and plot routine}
   SetStrucParBoundaries;
   DrawOWSensitivityPlot(False);
