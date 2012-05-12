33599a07f5a25265fe527810fbab2bfe2f58e303
jwdietrich <jwdietrich@18aeb308-97f3-4495-a81e-6d4c78a8ff35>
2012-05-12 13:27:14 +0000

    Additional preparative works on tornado plot window.

--- tornado.pas
+++ tornado.pas
@@ -14,8 +14,9 @@ unit tornado;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
-  TAGraph, TAStyles;
+  SimThyrTypes, Classes, SysUtils, FileUtil, LResources, Forms, Controls,
+  Graphics, Dialogs, StdCtrls, ExtCtrls, ComCtrls, TAGraph, TAStyles,
+  TASeries, TASources, TATools, SimThyrServices, Sensitivityanalysis;
 
 type
 
@@ -23,17 +24,93 @@ type
 
   TTornadoPlotForm = class(TForm)
     Chart1: TChart;
-    ChartStyles1: TChartStyles;
+    CheckGroup1: TCheckGroup;
+    ComboBox1: TComboBox;
+    FBar: TBarSeries;
+    StatusBar1: TStatusBar;
+    procedure FormCreate(Sender: TObject);
+    procedure FormShow(Sender: TObject);
   private
     { private declarations }
   public
     { public declarations }
-  end; 
+  end;
+
+  TNumberPair = record
+    l, u: real;
+  end;
 
 var
   TornadoPlotForm: TTornadoPlotForm;
+  gStrucPar, gDepPar: TNumberPair;
 
 implementation
+
+uses TACustomSeries;
+
+{ TTornadoPlotForm }
+
+procedure Rotate(ASeries: TBasicPointSeries);
+{rotates bars}
+var
+  t: integer;
+begin
+  with ASeries do
+  begin
+    t := AxisIndexX;
+    AxisIndexX := AxisIndexY;
+    AxisIndexY := t;
+  end;
+end;
+
+function TestPair(thePar, amount: real): TNumberPair;
+{creates a pair of test values from the given structure parameter and amount}
+var
+  tempResult: TNumberPair;
+begin
+  tempResult.l := (1 - amount) * thePar;
+  tempResult.u := (1 + amount) * thePar;
+  TestPair := tempResult;
+end;
+
+procedure DrawTornadoPlot;
+var
+  i, j: integer;
+begin
+  {If bar series exists it is cleared and recreated to support foundations of redrawing}
+  if TornadoPlotForm.FBar <> nil then
+    TornadoPlotForm.Chart1.Series.Clear;
+  TornadoPlotForm.FBar := TBarSeries.Create(TornadoPlotForm.Chart1);
+  TornadoPlotForm.Chart1.AddSeries(TornadoPlotForm.FBar);
+  with TornadoPlotForm.FBar do
+  begin
+    AxisIndexX := 1;
+    AxisIndexY := 0;
+    BarWidthPercent := 20;
+  end;
+  Rotate(TornadoPlotForm.FBar);
+  SaveStrucPars;
+  gstrucPar := TestPair(GD1, 0.2);
+  gDepPar.l := -30;
+  gDepPar.u := +30;
+  TornadoPlotForm.FBar.Add(gDepPar.l, '', clGray);
+  TornadoPlotForm.FBar.Add(gDepPar.u, '', clBlack);
+  gDepPar.l := -50;
+  gDepPar.u := +100;
+  TornadoPlotForm.FBar.Add(gDepPar.l, '', clGray);
+  TornadoPlotForm.FBar.Add(gDepPar.u, '', clBlack);
+  RestoreStrucPars;
+end;
+
+procedure TTornadoPlotForm.FormCreate(Sender: TObject);
+begin
+  DrawTornadoPlot;
+end;
+
+procedure TTornadoPlotForm.FormShow(Sender: TObject);
+begin
+  DrawTornadoPlot;
+end;
 
 initialization
   {$I tornado.lrs}
