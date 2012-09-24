2f298dc40f5ceab896b2b6bb217c1b51185dde3d
jwdietrich <jwdietrich@18aeb308-97f3-4495-a81e-6d4c78a8ff35>
2012-09-24 21:39:28 +0000

    First, still very frugal, export function for log data in DIF format.

--- difsupport.pas
+++ difsupport.pas
@@ -16,62 +16,106 @@ interface
 uses
   Classes, SysUtils;
 
+const
+  kCRLF = #13#10;
+  kQUOT = #34;
+
 type
   tHeader = Record
     version: Integer;
-    Identifier: String;
+    title: String;
     vectors, tuples: Integer;
   end;
   TDIFDocument = class
-    content: TStringList;
+    content: TStrings;
     Header: tHeader;
     public
       constructor Create;
       destructor Destroy; override;
       procedure SetHead(Identifier: String);
-      procedure AppendTuple(Value: String);
-      procedure AppendTuple(Value: Real);
-      procedure AppendTuple(Value: Boolean);
+      procedure NewTuple;
+      procedure AppendCell(Value: String);
+      procedure AppendCell(Value: Real);
+      procedure AppendCell(Value: Boolean);
   end;
 
 procedure WriteDIFFile(Doc: TDIFDocument; path: String);
+
+var
+  gNumVectors: integer;
 
 implementation
 
 constructor TDIFDocument.Create;
 begin
-  content.Create;
+  inherited Create;
+  content := TStringList.Create;
+  content.TextLineBreakStyle := tlbsCRLF;
 end;
 
 destructor TDIFDocument.Destroy;
 begin
-  content.Destroy;
+  content.Free;
   Inherited destroy;
 end;
 
 procedure TDIFDocument.SetHead(Identifier: String);
 begin
-  Header.Identifier := Identifier;
+  with Header do
+  begin
+  title := Identifier;
+  version := 1;
+  vectors := 0;
+  tuples := 0;
+  end;
 end;
 
-procedure TDIFDocument.AppendTuple(Value: String);
+procedure TDIFDocument.NewTuple;
 begin
-
+  gNumVectors := 0;
+  inc(header.tuples);
+  content.Append('-1,0');
+  content.Append('BOT');
 end;
 
-procedure TDIFDocument.AppendTuple(Value: Real);
+procedure TDIFDocument.AppendCell(Value: String);
 begin
-
+  inc(gNumVectors);
+  if gNumVectors > header.vectors then
+    header.vectors := gNumVectors;
+  content.Append('1,0');
+  content.Append(kQUOT + Value + kQUOT);
 end;
 
-procedure TDIFDocument.AppendTuple(Value: Boolean);
+procedure TDIFDocument.AppendCell(Value: Real);
 begin
+  AppendCell(FloatToStr(Value));
+end;
 
+procedure TDIFDocument.AppendCell(Value: Boolean);
+begin
+  if value = true then
+    AppendCell('TRUE')
+  else
+    AppendCell('FALSE');
 end;
 
 procedure WriteDIFFile(Doc: TDIFDocument; path: String);
+var
+  headerChunk, dataChunk, endChunk: String;
 begin
-
+  headerChunk := 'TABLE' + kCRLF + '0,' + IntToStr(Doc.Header.version) + kCRLF;
+  headerChunk := headerChunk + kQUOT + Doc.Header.title + kQUOT + kCRLF;
+  headerChunk := headerChunk + 'VECTORS' + kCRLF + '0,' + IntToStr(Doc.Header.vectors) + kCRLF + kQUOT + kQUOT + kCRLF;
+  headerChunk := headerChunk + 'TUPLES' + kCRLF + '0,' + IntToStr(Doc.Header.tuples) + kCRLF + kQUOT + kQUOT + kCRLF;
+  dataChunk := 'DATA' + kCRLF + '0,0' + kCRLF + kQUOT + kQUOT;
+  endChunk := '-1,0' + kCRLF + 'EOD';
+  Doc.content.Insert(0, headerChunk + dataChunk);
+  Doc.content.Append(endChunk);
+  try
+    Doc.content.SaveToFile(path);
+  finally
+  end;
 end;
 
 end.
