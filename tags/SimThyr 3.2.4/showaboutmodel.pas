3d2577598627199779ed503415812bd3e7c24716
jwdietrich <jwdietrich@18aeb308-97f3-4495-a81e-6d4c78a8ff35>
2013-11-02 15:45:02 +0000

    SimThyr 3.2.4 tagged.

---
+++ tags/SimThyr 3.2.4/showaboutmodel.pas
@@ -0,0 +1,75 @@
+unit ShowAboutModel;
+
+{ SimThyr Project }
+{ A numerical simulator of thyrotropic feedback control }
+
+{ Version 3.2.4 }
+
+{ (c) J. W. Dietrich, 1994 - 2013 }
+{ (c) Ludwig Maximilian University of Munich 1995 - 2002 }
+{ (c) Ruhr University of Bochum 2005 - 2013 }
+
+{ This unit draws a window with additional information and references }
+
+{ Source code released under the BSD License }
+{ See http://simthyr.sourceforge.net for details }
+
+{$mode objfpc}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
+  LCLIntf, StdCtrls;
+
+type
+
+  { TAboutModelForm }
+
+  TAboutModelForm = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    Memo1: TMemo;
+    StaticText2: TStaticText;
+    procedure Label1Click(Sender: TObject);
+    procedure Label2Click(Sender: TObject);
+    procedure Memo1Change(Sender: TObject);
+    procedure StaticText1Click(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end; 
+
+var
+  AboutModelForm: TAboutModelForm;
+
+implementation
+
+{ TAboutModelForm }
+
+procedure TAboutModelForm.Label1Click(Sender: TObject);
+begin
+
+end;
+
+procedure TAboutModelForm.Label2Click(Sender: TObject);
+begin
+  OpenURL('http://tfc.medical-cybernetics.de');
+end;
+
+procedure TAboutModelForm.Memo1Change(Sender: TObject);
+begin
+
+end;
+
+procedure TAboutModelForm.StaticText1Click(Sender: TObject);
+begin
+
+end;
+
+initialization
+  {$I showaboutmodel.lrs}
+
+end.
+
---
+++ tags/SimThyr 3.2.4/simoptions.pas
@@ -0,0 +1,125 @@
+unit SimOptions;
+
+{ SimThyr Project }
+{ A numerical simulator of thyrotropic feedback control }
+
+{ Version 3.2.4 }
+
+{ (c) J. W. Dietrich, 1994 - 2012 }
+{ (c) Ludwig Maximilian University of Munich 1995 - 2002 }
+{ (c) Ruhr University of Bochum 2005 - 2012 }
+
+{ This unit supports a small dialog with additional simulation settings }
+
+{ Source code released under the BSD License }
+{ See http://simthyr.sourceforge.net for details }
+
+{$mode objfpc}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
+  StdCtrls, SimThyrTypes;
+
+type
+
+  { TSimOptionsDlg }
+
+  TSimOptionsDlg = class(TForm)
+    CancelButton: TButton;
+    Label1: TLabel;
+    Label2: TLabel;
+    TRHEdit: TEdit;
+    OKButton: TButton;
+    NoiseCheckBox: TCheckBox;
+    PreviewCheckBox: TCheckBox;
+    procedure CancelButtonClick(Sender: TObject);
+    procedure FormShow(Sender: TObject);
+    procedure NoiseCheckBoxChange(Sender: TObject);
+    procedure OKButtonClick(Sender: TObject);
+    procedure PreviewCheckBoxChange(Sender: TObject);
+  private
+    { private declarations }
+    procedure CheckNoise;
+    procedure CheckPreview;
+  public
+    { public declarations }
+  end; 
+
+var
+  SimOptionsDlg: TSimOptionsDlg;
+  temp_noiseflag, temp_previewflag: boolean;
+
+implementation
+
+{ TSimOptionsDlg }
+
+procedure TSimOptionsDlg.CheckNoise;
+{flag for simulating noise}
+begin
+  if noiseflag then
+    NoiseCheckBox.Checked := true
+  else
+    NoiseCheckBox.Checked := false;
+end;
+
+procedure TSimOptionsDlg.CheckPreview;
+{flag for beginning simulation with predicted values}
+begin
+  if previewflag then
+    PreviewCheckBox.Checked := true
+  else
+    PreviewCheckBox.Checked := false;
+end;
+
+procedure TSimOptionsDlg.FormShow(Sender: TObject);
+{code executed on show}
+begin
+  temp_noiseflag := noiseflag;
+  temp_previewflag := previewflag;
+  TRHEdit.Text := FloatToStrF(TRHs, ffGeneral, 5, 2);
+  CheckNoise;
+  CheckPreview;
+  ShowOnTop;
+  SetFocus;
+end;
+
+procedure TSimOptionsDlg.CancelButtonClick(Sender: TObject);
+{closes dialog without reading any data}
+begin
+  SimOptionsDlg.Close;
+end;
+
+procedure TSimOptionsDlg.NoiseCheckBoxChange(Sender: TObject);
+{flag for simulating noise}
+begin
+  if NoiseCheckBox.Checked then
+    temp_noiseflag := true
+  else
+    temp_noiseflag := false;
+end;
+
+procedure TSimOptionsDlg.OKButtonClick(Sender: TObject);
+{closes dialog and accepts entered data}
+begin
+  noiseflag := temp_noiseflag;
+  previewflag := temp_previewflag;
+  TRHs := StrToFloat(TRHEdit.Text);
+  SimOptionsDlg.Close;
+end;
+
+procedure TSimOptionsDlg.PreviewCheckBoxChange(Sender: TObject);
+{flag for beginning simulation with predicted values}
+begin
+  if PreviewCheckBox.Checked then
+    temp_previewflag := true
+  else
+    temp_previewflag := false;
+end;
+
+initialization
+  {$I simoptions.lrs}
+
+end.
+
