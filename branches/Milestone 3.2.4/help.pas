d69abcb69a55a86034524c5196f3910b8f2c0eac
jwdietrich <jwdietrich@18aeb308-97f3-4495-a81e-6d4c78a8ff35>
2013-11-02 15:44:11 +0000

    Milestone 3.2.4 branched.

---
+++ branches/Milestone 3.2.4/help.pas
@@ -0,0 +1,87 @@
+unit help;
+
+{ SimThyr Project }
+{ A numerical simulator of thyrotropic feedback control }
+
+{ Version 3.2.4 }
+
+{ (c) J. W. Dietrich, 1994 - 2013 }
+{ (c) Ludwig Maximilian University of Munich 1995 - 2002 }
+{ (c) Ruhr University of Bochum 2005 - 2013 }
+
+{ This unit draws a help window with clickable options to get additional information }
+
+{ Source code released under the BSD License }
+{ See http://simthyr.sourceforge.net for details }
+
+{$mode objfpc}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
+  StdCtrls, LCLIntf, ExtCtrls, SimThyrTypes, SimThyrServices, ShowAboutModel,
+  AboutDialog;
+
+type
+
+  { THelpWindow }
+
+  THelpWindow = class(TForm)
+    SimThyrIconImage: TImage;
+    MainTitle: TLabel;
+    OnlineHelpSubTitle: TLabel;
+    ModelInfoTitle: TLabel;
+    OnlineHelpTitle: TLabel;
+    ModelInfoSubTitle: TLabel;
+    VersionInfoTitle: TLabel;
+    VersionInfoSubTitle: TLabel;
+    procedure FormCreate(Sender: TObject);
+    procedure ModelInfoTitleClick(Sender: TObject);
+    procedure OnlineHelpTitleClick(Sender: TObject);
+    procedure VersionInfoTitleClick(Sender: TObject);
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  HelpWindow: THelpWindow;
+
+implementation
+
+{ THelpWindow }
+
+procedure THelpWindow.OnlineHelpTitleClick(Sender: TObject);
+begin
+  OpenURL(HELP_URL);
+end;
+
+procedure THelpWindow.VersionInfoTitleClick(Sender: TObject);
+begin
+  HelpWindow.Close;
+  AboutWindow.ShowAbout;
+end;
+
+procedure THelpWindow.ModelInfoTitleClick(Sender: TObject);
+begin
+  AboutModelForm.ShowOnTop;
+  HelpWindow.Close;
+end;
+
+procedure THelpWindow.FormCreate(Sender: TObject);
+begin
+  {$IFDEF win32}
+    {adapt font sizes to be readable also under Windows}
+    OnlineHelpSubTitle.Font.Height := 12;
+    ModelInfoSubTitle.Font.Height := 12;
+    VersionInfoSubTitle.Font.Height := 12;
+  {$ENDIF}
+end;
+
+initialization
+  {$I help.lrs}
+
+end.
+
