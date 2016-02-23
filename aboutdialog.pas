378d521e1f262c3e190375f771e5a1173d4cef74
jwdietrich <jwdietrich@18aeb308-97f3-4495-a81e-6d4c78a8ff35>
2016-02-23 20:05:39 +0000

    Optimising chart display for Mac OS X. Copyright information updated.

--- aboutdialog.pas
+++ aboutdialog.pas
@@ -5,9 +5,9 @@ unit AboutDialog;
 
 { Version 4.0.0 (Merlion) }
 
-{ (c) J. W. Dietrich, 1994 - 2015 }
+{ (c) J. W. Dietrich, 1994 - 2016 }
 { (c) Ludwig Maximilian University of Munich 1995 - 2002 }
-{ (c) Ruhr University of Bochum 2005 - 2015 }
+{ (c) Ruhr University of Bochum 2005 - 2016 }
 
 { This unit implements an about-box }
 
